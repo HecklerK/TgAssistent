bd24f5a3a91890195b24341cab97b8481c070dfe
CyxPi-PC <artem2802200@yandex.ru>
2025-04-16 17:09:29 +0300

    next

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ bot = Client(name=config('LOGIN'),
 async def isNotFakeScam(client: Client, message: Message):
     user = message.from_user
     if (user.is_fake | user.is_scam):
-        await client.send_message(chat_id=message.chat.id, text="Вы помечены как фейк/скам")
+        await client.send_message(chat_id="me", text=f"Пользователь {user.username} помечен как фейк/скам")
         return False
     
     return True
@@ -29,9 +29,34 @@ async def handle_message(client: Client, message: Message):
     text = message.text.lower()
 
     if (text == '\U00002754') & await isNotFakeScam(client, message):
-        await client.send_message(chat_id=message.chat.id, text=config('ABOUT_ME'))
+        answer = await message.reply(text=config('ABOUT_ME'), quote=True)
+        await client.pin_chat_message(answer.chat.id, answer.id)
     elif (re.search(r'расскажи о себе|расскажешь о себе', text) != None) & (not message.from_user.is_self) & await isNotFakeScam(client, message):
-        await client.send_message(chat_id=message.chat.id, text=('Автоматическое сообщение: ' + config('ABOUT_ME')))
+        answer = await message.reply(chat_id=message.chat.id, text=('Автоматическое сообщение: ' + config('ABOUT_ME')), quote=True)
+        await client.pin_chat_message(answer.chat.id, answer.id)
+    
+    if message.from_user.is_self & ("\U00002754 " in text):
+        user_id = text.split("\U00002754 ")[1].strip()
+        user = await client.get_users(user_id)
+
+        if user != None:
+            flags = ''
+            if user.is_scam:
+                flags += "Скам"
+            if user.is_fake:
+                flags += "Фейк"
+            if user.is_premium:
+                flags += "Премиум"
+            if user.is_verified:
+                flags += "Верифицирован"
+            if user.is_support:
+                flags += "Telegram support"
+
+            last_online = ''
+            if user.last_online_date != None:
+                user.last_online_date != None
+
+            await message.reply(text=f'Пользователь {user.username},\nИмя {user.first_name},\nФамилия {user.last_name},\nМетки {flags},\nКод страны {user.language_code},\nПоследняя дата в сети {last_online}', quote=True)
 
 
 bot.run()
